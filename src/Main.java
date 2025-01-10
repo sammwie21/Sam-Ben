0f3f15a7c42d64f93fcdb55ca44b098097863284
BT_1N3_13 <sample@bths.edu>
2025-01-10 13:45:23 -0500

    for benson wu

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Ben-Sam!");
         System.out.println("We are very excited to work on this together!");
-        System.out.println("Sigma boy");
+        System.out.println("chopped chin");
         System.out.println("Imagine if benson wu got a low taper fade");
     }
 }
