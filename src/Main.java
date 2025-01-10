2d77f2fd9e52e228efe2b910ad1d194684457c4b
BT_1N3_13 <sample@bths.edu>
2025-01-10 13:43:11 -0500

    lower taper fade

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Welcome to Sam-Ben!");
         System.out.println("We are very excited to work on this together!");
         System.out.println("Sigma boy");
-        System.out.println("Imagine if samuel park got a low taper fade");
+        System.out.println("Imagine if benson wu got a low taper fade");
     }
 }
