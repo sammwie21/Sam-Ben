8f2a2cb0a2ca91b7cfec7cbc954c1a9eba5a0d6b
BT_1N3_14 <sample@bths.edu>
2025-01-10 13:35:16 -0500

    added print statement

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Sam-Ben!");
+        System.out.println("We are very excited to work on this together!");
     }
 }
