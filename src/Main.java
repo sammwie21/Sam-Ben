8bf31eb5f82ab89f7615ec734645a44dde2631de
BT_1N3_14 <sample@bths.edu>
2025-01-10 13:43:13 -0500

    changed first print statement

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Sam-Ben!");
+        System.out.println("Welcome to Ben-Sam!");
         System.out.println("We are very excited to work on this together!");
         System.out.println("Sigma boy");
         System.out.println("Imagine if samuel park got a low taper fade");
