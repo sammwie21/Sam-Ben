2245fd7690f9f956f94df44d2868b280f934974c
BT_1N3_13 <sample@bths.edu>
2025-01-10 13:38:47 -0500

    Third

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Sam-Ben!");
         System.out.println("We are very excited to work on this together!");
+        System.out.println("Sigma boy");
     }
 }
---
+++ src/Simple.java
@@ -0,0 +1,6 @@
+public class Simple {
+    private String mh;
+    public Simple(){
+        mh = "ily";
+    }
+}
